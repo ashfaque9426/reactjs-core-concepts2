9bb986c74dd80388c6d356c73362072f0a893fab
ashfaque9426 <ashfaqsylvi9426@gmail.com>
2023-03-20 23:43:48 +0600

    react core concepts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // const products = [
+  //   {name: "laptop", price: 53000},
+  //   {name: "iphone", price: 75000},
+  //   {name: "watch", price: 33000},
+  //   {name: "tablet", price: 9000},
+  //   {name: "sunglass", price: 5900},
+  // ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="App grid">
+      {/* {
+        products.map(product => <Product name={product.name} price={product.price}></Product>)
+      } */}
+      <Counter></Counter>
+    </section>
   );
+}
+
+// function Product(props) {
+//   return (
+//     <article className='product'>
+//       <h3>Name: {props.name}</h3>
+//       <p>Price: {props.price}</p>
+//     </article>
+//   );
+// }
+
+function Counter() {
+  let [count, setCount] = useState(0);
+  const increaseCount = () => setCount(++count);
+  const decreaseCount = () => setCount(--count);
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  )
 }
 
 export default App;
